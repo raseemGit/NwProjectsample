f5346cb722ad6f555517e7ca125cb01fb54810fa
raseemGit <raseem.moha@gmail.com>
2019-10-02 13:02:45 +0530

    check

---
+++ src/org/text/A.java
@@ -0,0 +1,27 @@
+package org.text;
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+		
+	String s2= "MohamedaRaseem";
+	
+	 		for (int i = 0; i < s2.length()-6; i++) {
+	 			char val = s2.charAt(i);
+	 			System.out.print(val);
+				
+			}
+	
+	
+	
+			
+			
+	}
+		
+		
+		
+		
+
+	
+}
